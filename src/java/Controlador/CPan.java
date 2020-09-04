c4be616d62fed79c288315cc4ada92ee85893b84
Demonassit <demonassit@hotmail.com>
2020-09-03 19:13:22 -0500

    se agrego la clase CPan

---
+++ src/java/Controlador/CPan.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+/**
+ *
+ * @author demon
+ */
+public class CPan {
+    
+    private int id_cpan;
+    private String nom_cpan;
+    
+    public CPan(){
+    
+    }
+
+    public int getId_cpan() {
+        return id_cpan;
+    }
+
+    public void setId_cpan(int id_cpan) {
+        this.id_cpan = id_cpan;
+    }
+
+    public String getNom_cpan() {
+        return nom_cpan;
+    }
+
+    public void setNom_cpan(String nom_cpan) {
+        this.nom_cpan = nom_cpan;
+    }
+    
+    
+    
+}
