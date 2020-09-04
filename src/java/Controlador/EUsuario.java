b229d4a49cf31282aa4c15231dcc9f63e95e5f7e
Demonassit <demonassit@hotmail.com>
2020-09-03 19:12:15 -0500

    se agrego la clase CSaborPan

---
+++ src/java/Controlador/EUsuario.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+/**
+ *
+ * @author demon
+ */
+public class EUsuario {
+    
+    private int id_eusu, id_usu;
+    
+    //constructor
+    
+    public EUsuario(){
+    
+    }
+
+    public int getId_eusu() {
+        return id_eusu;
+    }
+
+    public void setId_eusu(int id_eusu) {
+        this.id_eusu = id_eusu;
+    }
+
+    public int getId_usu() {
+        return id_usu;
+    }
+
+    public void setId_usu(int id_usu) {
+        this.id_usu = id_usu;
+    }
+    
+    
+    
+    
+}
